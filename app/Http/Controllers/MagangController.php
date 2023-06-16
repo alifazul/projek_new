29eb780fb3f97ec19eeacafa7dc5586bb9984a25
alifazul <102244146+alifazul@users.noreply.github.com>
2023-06-16 10:21:42 +0700

    Update MagangController.php

--- app/Http/Controllers/MagangController.php
+++ app/Http/Controllers/MagangController.php
@@ -13,22 +13,86 @@ use Auth;
 
 class MagangController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index(): View
     {
         $magang = Magang::latest()->paginate(7);
         return view('admin.tabelmagang', compact('magang'));
     }
-
+    
     public function create(): View
     {
         $user_id = Auth::user()->id;
-        return view('admin.tambah_magang', compact('user_id'));
+         return view('admin.tambah_magang', compact('user_id'));
     }
-
+    public function tambah(): View
+    {
+        $user_id = Auth::user()->id;
+        $magang = Magang::where('user_id',$user_id)->get();
+        
+        return view('layouts.tambahmagang', compact('user_id','magang'));
+    }
+    public function getmagang(): View
+    {
+        $magang = Magang::where('status','pending')->get()->paginate(5);
+        return view('admin.tabelmagang', compact('magang'));
+    }
+    
     public function show($id): View
     {
         $data_magang = Magang::findOrFail($id);
         return view('admin.detail_magang',compact('data_magang'));
+    }
+
+    public function simpan(Request $request): RedirectResponse
+    {
+        $this->validate($request,[
+            'posisi'=>'required',
+            'perusahaan'=>'required',
+            'pendidikan'=>'required',
+            'tipe'=>'required',
+            'sertifikat'=>'required',
+            'durasi'=>'required',
+            'deadline'=>'required',
+            'pengumuman'=>'required',
+            'mulai_magang'=>'required',
+            'provinsi'=>'required',
+            'kabupaten'=>'required',
+            'link_daftar'=>'required',
+            'status'=>'required',
+        ]);
+
+        Magang::create([
+            'user_id'=>$request->id_user,
+            'posisi' => $request->posisi,
+            'perusahaan' => $request->perusahaan,
+            'pendidikan' => $request->pendidikan,
+            'tipe' => $request->tipe,
+            'sertifikat' => $request->sertifikat,
+            'durasi' => $request->durasi,
+            'deadline' => $request->deadline,
+            'pengumuman' => $request->pengumuman,
+            'mulai_magang' => $request->mulai_magang,
+            'provinsi' => $request->provinsi,
+            'kabupaten' => $request->kabupaten,
+            'link_daftar' => $request->link_daftar,
+            'kontak' => $request->kontak,
+            'deskripsi' => $request->deskripsi,
+            'status'=>$request->status
+        ]);
+
+        $status=$request->status;
+        if($status=='publish'){
+        Alert::success('Selamat', 'Data Berhasil Dipublish');
+        }else if($status=='draft'){
+            Alert::success('Selamat', 'Data Berhasil Disimpan di Draft');
+        }else if($status=='pending'){
+            Alert::success('Selamat', 'Data Berhasil Dikirim');
+        }
+        return redirect()->route('profile.edit');
     }
 
     public function store(Request $request): RedirectResponse
@@ -116,6 +180,7 @@ class MagangController extends Controller
             'provinsi' => $request->provinsi,
             'kabupaten' => $request->kabupaten,
             'deskripsi' => $request->deskripsi,
+            'status'=>$request->status
         ]);
         Alert::success('Berhasil', 'Data Berhasil Diubah');
         return redirect()->route('magang.index');
