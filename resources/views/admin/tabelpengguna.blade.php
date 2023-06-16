6f1ae98205a9414a9300c9519137847a8e835e17
alifazul <102244146+alifazul@users.noreply.github.com>
2023-06-16 10:35:26 +0700

    Update tabelpengguna.blade.php

--- resources/views/admin/tabelpengguna.blade.php
+++ resources/views/admin/tabelpengguna.blade.php
@@ -1,10 +1,15 @@
 @extends('admin.template')
 
 <?php $title='Data Pengguna';
-$header='Data Pengguna'; ?>
+$header='Data Pengguna'; 
+$no=1;?>
+@section('datatable')
+ <!-- Custom styles for this page -->
+ <link href="{{ asset('admin/vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+ @endsection
 
 @section('tomboltambah')
-<a href="#" class="d-none d-sm-inline-block btn btn-lg btn-success shadow-sm">Tambah</a>
+<a href="{{ route('user.create') }}" class="d-none d-sm-inline-block btn btn-lg btn-success shadow-sm">Tambah</a>
 @endsection
 <!-- DataTales Example -->
 @section('content')
@@ -21,34 +26,44 @@ $header='Data Pengguna'; ?>
                         <th>Nama</th>
                         <th>Email</th>
                         <th>No. HP</th>
-                        <th>Foto</th>
+                        
                         <th>Aksi</th>
                     </tr>
                 </thead>
                 <tbody>
-                
+                @foreach($pengguna as $a)
                     <tr>
-                        
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>{{ $no }}</td>
+                        <td>{{ $a->nama }}</td>
+                        <td>{{ $a->email }}</td>
+                        <td>{{ $a->no_hp }}</td>
                         <td class="text-center">
                             
-                            <a href="{{ route('pengguna.edit', $a->id) }}" class="btn btn-sm btn-success">
+                            <a href="{{ route('user.edit', $a->id) }}" class="btn btn-sm btn-success">
                             <i class="fas fa-edit"></i>Edit</a>
                             @method('DELETE')
-                            <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('magang.destroy', $a->id) }}" method="POST">
+                            <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('user.destroy', $a->id) }}" method="GET">
                                 <button type="submit" class="btn btn-sm btn-danger">                     <i class="fas fa-trash"></i>Delete</button>
                             </form>
                         </td>
+                        <?php $no++; ?>
+                       
                     </tr>
-                    <?php $no++; ?>
+                    @endforeach
+                   
                     
                 </tbody>
             </table>
         </div>
     </div>
 </div>
+@endsection
+
+@section('tabelj')
+<!-- Page level plugins -->
+<script src="{{ asset('admin/vendor/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('admin/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="{{ asset('admin/js/demo/datatables-demo.js') }}"></script>
 @endsection
