6fb288c223898bc1f752d8c3d49c8ea1ba77aeaa
delana <85440817+raihan27-cyber@users.noreply.github.com>
2023-06-16 12:19:12 +0700

    Add files via upload

--- resources/views/carimagang.blade.php
+++ resources/views/carimagang.blade.php
@@ -79,10 +79,55 @@
 </div>
     </div>
   </div>
-
- 
-
 </section>
+
+<section id="terbaru" class="mt-5">
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col-12 text-center">
+                    <h2>Lowongan Magang</h2>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-start">
+                        <div class="card-body me-3 text-center">
+                          <div class="position-relative top-50 start-50 translate-middle">
+                          <h4>Magang United Tractor</h4>
+                            <p>Staf Administrasi
+                                Perum Lembaga Penyelenggara Pelayanan Navigasi 
+                                Penerbangan Indonesia • Kota Tangerang</p>
+                            <button class="button-magang">Selengkapnya</button>
+                          </div>
+                        </div>
+
+                        <div class="card-body me-3">
+                          <div class="position-relative top-50 start-50 translate-middle">
+                          <h4>Magang United Tractor</h4>
+                            <p>Staf Administrasi
+                                Perum Lembaga Penyelenggara Pelayanan Navigasi 
+                                Penerbangan Indonesia • Kota Tangerang</p>
+                            <button  class="button-magang">Selengkapnya</button>
+                          </div>
+                        </div>
+
+                        <div class="card-body me-3">
+                          <div class="position-relative top-50 start-50 translate-middle">
+                          <h4>Magang United Tractor</h4>
+                            <p>Staf Administrasi
+                                Perum Lembaga Penyelenggara Pelayanan Navigasi 
+                                Penerbangan Indonesia • Kota Tangerang</p>
+                            <button class="button-magang">Selengkapnya</button>
+                          </div>
+                        </div>
+
+                        
+                    </div>
+                </div>
+</div>
+        </div>
+    </section>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
   </body>
