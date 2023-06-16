612ad115f231637a70bcff0be193b78cf21dc680
alifazul <102244146+alifazul@users.noreply.github.com>
2023-06-16 10:38:57 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MagangController;
-use App\Models\Magang;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\LihatMagangController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\HomeController;
+use App\Http\Controllers\PasswordController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +21,8 @@ use App\Models\Magang;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+
 
 Route::get('/home', function () {
     return view('home');
@@ -29,23 +37,21 @@ Route::get('/carimagang', function () {
 });
 
 Route::get('/login', function () {
-    return view('auth.login');
-});
-
-Route::get('/register', function () {
-    return view('auth.register');
+    return view('auth.login')->name('login');
 });
 
 Auth::routes();
 
-
+Route::get('/carimagang', [LihatMagangController::class, 'lihat'])->name('magang.lihat');
 
 Route::group(['middleware' => ['role:admin']],function () {
-    Route::get('/admin', function () {
-        return view('admin.index');
-    })->name('admin.dashboard');
+    Route::controller(AdminController::class)->prefix('adm')->group(function(){
+        Route::get('', 'index')->name('admin.index');
+    });
     Route::controller(MagangController::class)->prefix('admin/magang')->group(function(){
         Route::get('', 'index')->name('magang.index');
+        Route::get('create', 'create')->name('magang.create');
+        Route::post('store', 'store')->name('magang.store');
         Route::get('detail/{id}', 'show')->name('magang.show');
         Route::get('edit/{id}', 'edit')->name('magang.edit');
         Route::post('edit/{id}', 'update')->name('magang.update');
@@ -53,6 +59,8 @@ Route::group(['middleware' => ['role:admin']],function () {
     });
     Route::controller(ProfileController::class)->prefix('admin/user')->group(function(){
         Route::get('', 'index')->name('user.index');
+        Route::get('create', 'create')->name('user.create');
+        Route::post('store', 'store')->name('user.store');
         Route::get('detail/{id}', 'show')->name('user.show');
         Route::get('edit/{id}', 'edit')->name('user.edit');
         Route::post('edit/{id}', 'update')->name('user.update');
@@ -61,9 +69,16 @@ Route::group(['middleware' => ['role:admin']],function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profil', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/home', [ProfileController::class, 'logprof'])->name('home');
+    Route::get('/profil', [ProfileController::class, 'profil'])->name('profile');
+    Route::get('/editprofil', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::post('/editprofil', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profil', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/magang/tambah', [MagangController::class,'create'])->name('magang.create');
-    Route::post('/magang/tambah', [MagangController::class,'store'])->name('magang.store');
+    Route::get('/magang/tambah', [MagangController::class,'tambah'])->name('magang.tambah');
+    Route::post('/magang/tambah', [MagangController::class,'simpan'])->name('magang.simpan');
+    Route::get('password', [PasswordController::class,'edit'])->name('user.password.edit');
+    Route::patch('password', [PasswordController::class,'update'])->name('user.password.update');
+    Route::get('profile', [ProfileController::class,'editu'])->name('profile.edit');
+    Route::patch('profile', [ProfileController::class,'updateu'])->name('profile.update');
 });
+
